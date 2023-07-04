96457f06695a5c512b7bbf2edea7edc5986288cf
William Silva <williamreis.wsilva@gmail.com>
2023-07-04 13:48:33 -0300

    criando o hero e importando a fonte

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,23 @@
 const gulp = require("gulp"); // importação gulp
 const sass = require("gulp-sass")(require("sass")); // importação sass
+const imagemin = require("gulp-imagemin");
 
 // função para compilar arquivos de scss para css
 function styles(){
     return gulp.src("./src/styles/*scss") // capturar os arquivos
     .pipe(sass({outputStyle: "compressed"})) // stilo de saída dos arquivos css serão comprimidos
-    .pipe(gulp.dest("./dist/css")) // endereço destino dos arquivos de saída
+    .pipe(gulp.dest("./dist/css")) // endereço destino dos arquivos de saída ( pasta criada automatica)
 }
 
 
-exports.default = styles;
+// função para compilar arquivos de scss para css
+function images(){
+    return gulp.src("./src/images/**/*") // capturar todos arquivos de imagens em pastas
+    .pipe(imagemin()) // função de conpressão das imagens
+    .pipe(gulp.dest("./dist/images")) // endereço destino dos arquivos de saída ( pasta criada automatica)
+}
+
+exports.default = gulp.parallel(styles, images);
 
 // função para compilar arquivos em tempo real
 exports.watch = function(){
